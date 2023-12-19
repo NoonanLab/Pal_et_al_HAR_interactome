c8f2c2d9546485a3c3011dc721adcb5a304c11d9
Atreyo Pal <atreyo.pal@yale.edu>
2023-12-18 23:01:52 -0500

    HiCUP_preprocessing.sh updated

---
+++ HiCUP_preprocessing.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --job-name=Hicup_preprocessing
+#SBATCH --time=4-00:00:00
+#SBATCH --mem-per-cpu=7G
+
+module load R
+module load Bowtie2
+module load SAMtools
+
+# Full path to the conf file
+conf_file="$1"
+
+# Create the "results" directory if it doesn't exist
+mkdir -p results
+
+# Change to the "results" directory
+cd results
+
+# Read the conf file and print its parameters
+while IFS= read -r line; do
+    echo "$line"
+done < "$conf_file"
+
+# Link to the folder where hicup package is downloaded
+ln -s ~/bin/HiCUP-0.8.2
+
+# Run all preprocessing steps of HiCUP with the specified config file
+./hicup --config "$conf_file"
+
+# Link to the folder where umi-tools package is downloaded
+ln -s ~/bin/umi_tools
+
+# Run deduplication on the filtered reads (here named R1_2.filt.sort.bam) to remove PCR duplicates
+umi_tools dedup -I R1_2.filt.sort.bam --paired --umi-separator=":" -S R1_2.dedup.bam
